f1ca5f44478532a2ceba6817b6d820b84bb1775b
vitordiniz25 <vitordiniz25@gmail.com>
2024-05-15 21:43:04 -0300

    chore: config pytest

--- app/database.py
+++ app/database.py
@@ -5,24 +5,28 @@ import time
 class Database:
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls, test=False, test_cursor=None, test_conn=None):
         if cls._instance is None:
             cls._instance = super(Database, cls).__new__(cls)
             cls._instance._conn = None
             cls._instance._cursor = None
-            cls._instance._connect_db()
+            cls._instance._connect_db(test, test_cursor, test_conn)
         return cls._instance
 
-    def _connect_db(self):
+    def _connect_db(self, test, test_cursor, test_conn):
         try:
-            self._conn = psycopg2.connect(
-                host="localhost",
-                database="datamed",
-                user="postgres",
-                password="123",
-                cursor_factory=RealDictCursor
-            )
-            self._cursor = self._conn.cursor()
+            if test:
+                self._conn = test_conn
+                self._cursor = test_cursor
+            else:
+                self._conn = psycopg2.connect(
+                    host="localhost",
+                    database="datamed",
+                    user="postgres",
+                    password="123",
+                    cursor_factory=RealDictCursor
+                )
+                self._cursor = self._conn.cursor()
             print("Connected to the database")
         except Exception as error:
             print("Error connecting to the database")
@@ -36,8 +40,8 @@ class Database:
         return self._cursor
 
 # Functions to get the singleton instance's connection and cursor
-def get_conn():
-    return Database().get_conn()
+def get_conn(test=False, test_cursor=None, test_conn=None):
+    return Database(test, test_cursor, test_conn).get_conn()
 
-def get_cursor():
-    return Database().get_cursor()
+def get_cursor(test=False, test_cursor=None, test_conn=None):
+    return Database(test, test_cursor, test_conn).get_cursor()
