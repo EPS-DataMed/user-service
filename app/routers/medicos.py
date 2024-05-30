730dd351a795eb150dc4a79b21aab42b202c1b14
vitordiniz25 <vitordiniz25@gmail.com>
2024-05-29 22:59:18 -0300

    update routers for medicos and pacientes

--- app/routers/medicos.py
+++ app/routers/medicos.py
@@ -1,13 +1,35 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from .. import schemas, crud
+from ..schemas import MedicoCreate
+from ..services import medico as medico_service
 from ..database import get_db
 
 router = APIRouter()
 
-@router.post("/medicos/", response_model=schemas.MedicoCreate)
-def create_medico(medico: schemas.MedicoCreate, db: Session = Depends(get_db)):
-    db_medico = crud.get_paciente_by_email(db, email=medico.email)
+@router.post("/medicos/", response_model=MedicoCreate)
+def create_medico(medico: MedicoCreate, db: Session = Depends(get_db)):
+    db_medico = medico_service.get_medico_by_email(db, email=medico.email)
     if db_medico:
         raise HTTPException(status_code=400, detail="Email já registrado")
-    return crud.create_medico(db=db, medico=medico)
+    return medico_service.create_medico(db=db, medico=medico)
+
+@router.get("/medicos/{id_usuario}", response_model=MedicoCreate)
+def read_medico(id_usuario: int, db: Session = Depends(get_db)):
+    db_medico = medico_service.get_medico_by_id(db, id_usuario=id_usuario)
+    if db_medico is None:
+        raise HTTPException(status_code=404, detail="Medico não encontrado")
+    return db_medico
+
+@router.put("/medicos/{id_usuario}", response_model=MedicoCreate)
+def update_medico(id_usuario: int, medico: MedicoCreate, db: Session = Depends(get_db)):
+    db_medico = medico_service.update_medico(db, id_usuario=id_usuario, medico=medico)
+    if db_medico is None:
+        raise HTTPException(status_code=404, detail="Medico não encontrado")
+    return db_medico
+
+@router.delete("/medicos/{id_usuario}", response_model=MedicoCreate)
+def delete_medico(id_usuario: int, db: Session = Depends(get_db)):
+    db_medico = medico_service.delete_medico(db, id_usuario=id_usuario)
+    if db_medico is None:
+        raise HTTPException(status_code=404, detail="Medico não encontrado")
+    return db_medico
