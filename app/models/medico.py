64545b10382ffcc14af1c49ad1b9e9bbcb81dd7e
vitordiniz25 <vitordiniz25@gmail.com>
2024-05-30 18:44:41 -0300

    update models

--- app/models/medico.py
+++ app/models/medico.py
@@ -1,13 +1,11 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from .base import Base
 
 class Medico(Base):
     __tablename__ = "medicos"
-    id_usuario = Column(Integer, primary_key=True, index=True)
-    crm = Column(String, unique=True, nullable=False)
+    id_usuario = Column(Integer, ForeignKey('usuarios.id'), primary_key=True, index=True)
+    crm = Column(String, nullable=False)
     especialidade = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    senha = Column(String, nullable=False)
-    nome = Column(String, nullable=False)
-    dt_nascimento = Column(Date, nullable=False)
-    criado_em = Column(Date, nullable=False)
+
+    usuario = relationship("Usuario", back_populates="medico")
