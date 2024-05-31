06aa80a01e2625002c6e7a838dc0b239a3400e96
AntonioRangelC <antonio.rangel.02@gmail.com>
2024-05-31 15:28:19 -0300

    Alteração na configuração do CORS

--- app/main.py
+++ app/main.py
@@ -5,10 +5,23 @@ from sqlalchemy.exc import IntegrityError
 from typing import List
 from . import models, schemas, database
 import bcrypt
-
+from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = database.SessionLocal()
