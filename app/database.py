8a61d86e61960ca29efc94dbd802d8e36e247955
vitordiniz25 <vitordiniz25@gmail.com>
2024-05-31 08:27:34 -0300

    database config

--- app/database.py
+++ app/database.py
@@ -1,19 +1,10 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
-from .models.base import Base
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:postgres@localhost/datamed"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+Base = declarative_base()
