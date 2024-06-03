1cc51520f392faf1c52c294e71f42bd7be7d652a
VictorAmaralC <victoramaralce@gmail.com>
2024-06-03 11:00:52 -0300

    fix: hashing password
    fixing hash method

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,6 @@
-from fastapi import HTTPException, status
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
-from typing import List
-from . import models, schemas
-import bcrypt
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
+    return pwd_context.hash(password)
