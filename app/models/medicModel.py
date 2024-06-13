9d6df910a797118a9489dce35616ac4e0516ce4d
vitordiniz25 <vitordiniz25@gmail.com>
2024-06-12 22:37:55 -0300

    refactor: rename medic to doctor

--- app/models/medicModel.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from ..database import Base
-
-class Medico(Base):
-    __tablename__ = "medicos"
-
-    id_usuario = Column(Integer, ForeignKey("usuarios.id"), primary_key=True)
-    crm = Column(String(50), nullable=False)
-    especialidade = Column(String(255), nullable=False)
